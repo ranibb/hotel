abc36a9d2baecf093d8538fc32324fd42b6e0706
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-18 13:25:45 +0300

    Renaming a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { AccomodationComponent } from './accomodation/accomodation.component';
 import { RoomsComponent } from './accomodation/rooms/rooms.component';
-import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
-import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
-import { BarsComponent } from './food-beverage/bars/bars.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-and-beverage/bars/bars.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { WelnessAndSpaComponent } from './lifestyle/welness-and-spa/welness-and-spa.component';
 import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
@@ -28,7 +28,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     AppComponent,
     AccomodationComponent,
     RoomsComponent,
-    FoodBeverageComponent,
+    FoodAndBeverageComponent,
     RestaurantComponent,
     BarsComponent,
     LifestyleComponent,
