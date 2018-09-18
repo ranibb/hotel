e1be43418de759d7bba3bedd9653ce0722085335
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-18 20:31:36 +0300

    header and sidenav initial implementation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import {
     MatBadgeModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
 } from '@angular/material';
 
 @NgModule({
-    imports: [MatBadgeModule, MatButtonModule],
-    exports: [MatBadgeModule, MatButtonModule]
+    imports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+    ],
+    exports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+    ]
 })
 export class MaterialModule { }
